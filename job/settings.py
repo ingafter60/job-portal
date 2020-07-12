c645df39d7d328ba989c5b224e5f9f2f8f900a79
ing <inyoman_gurnitha@yahoo.com>
2020-07-13 01:40:25 +0700

    6.  104. Creating Static Files Directory

--- job/settings.py
+++ job/settings.py
@@ -1,3 +1,6 @@
+''' job/settings.py'''
+
+
 """
 Django settings for job project.
 
@@ -129,5 +132,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
