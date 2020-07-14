6b3d7365776aba06f99e2769944c697deb4a72bb
ing <inyoman_gurnitha@yahoo.com>
2020-07-14 12:01:44 +0700

    16. 114. How To Render/List Jobs using GCBV

--- jobs/views.py
+++ jobs/views.py
@@ -1,9 +1,22 @@
 # jobs/views.py
 
+# DJANGO MODULES
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 
-class HomeView(TemplateView):
+# IMPORT MODELS
+from jobs.models import Job
 
+# # HomeView using TemplateView
+# class HomeView(TemplateView):
+# 	template_name = 'jobs/index.html'
+
+# HomeView using ListView
+class HomeView(ListView):
+	'''use Job model'''
+	model = Job 
+	'''use context to store objects in a variable called jobs'''
+	context_object_name = 'jobs'
+	'''template name to use to render the job objects'''
 	template_name = 'jobs/index.html'
